f1f0ba7f0d64813e4c49cd72c8a306a160fd4420
charliedolphin <dachistyakov@ya.ru>
2017-03-15 17:38:38 +0300

    Sort and preliminary calc of exp
    Added sorting of output data and function of preliminary calculation of
    complex exponent.

--- radix4.c
+++ radix4.c
@@ -4,6 +4,8 @@
 //
 // Updates:
 //      11.03.2017 - release.
+//      15.03.2017 - added sorting of output data, added preliminary calculation
+// of complex exponents.
 //*****************************************************************************
 //*****************************************************************************
 #include <stdio.h>
@@ -19,18 +21,16 @@ float FFT_j[64];
 float exp_r[64];
 float exp_j[64];
 
-// Intermediate values 
 float   rimd0, jimd0,
         rimd1, jimd1,
         rimd2, jimd2,
         rimd3, jimd3;
 
-float pi = 3.1415926535897932384626433832795;
-int n;
-int step;
-int i, kk;
-unsigned int N = 64;
-float arg;
+
+int i, kk, n, step;
+int N = 64;
+int d = 0;
+
 
 
 // REAL coefficients of butterfly
@@ -63,8 +63,33 @@ float m_im (float x1, float y1, float x2, float y2)
     return result;
 }
 
+void FFT_ExpCalculation()
+{
+    float arg;
+    float pi = 3.1415926535897932384626433832795;
+    int n;
+    for (n=0; n<16; n++){
+        arg = 0;
+        exp_r[0+n] = 1;
+        exp_j[0+n] = 0;
+
+        arg = (float)(2*pi*n)/64.0;
+        exp_r[16+n] = cos(arg); 
+        exp_j[16+n] = sin(-arg);
+
+        arg = arg * 2;
+        exp_r[32+n] = cos(arg); 
+        exp_j[32+n] = sin(-arg);
+
+        arg = arg * 3 / 2;
+        exp_r[48+n] = cos(arg); 
+        exp_j[48+n] = sin(-arg);
+    }
+}
+
 int main()
 {
+    FFT_ExpCalculation();
 
     for (i=0; i<N; i++){
         // input data fills "fr" array
@@ -120,68 +145,53 @@ int main()
                     m_real(bat_s4r[1], bat_s4j[1], rimd1, jimd1)+
                     m_real(bat_s4r[2], bat_s4j[2], rimd2, jimd2)+
                     m_real(bat_s4r[3], bat_s4j[3], rimd3, jimd3);
-                    FFT_j[index3] = m_im(bat_s4r[0], bat_s4j[0], rimd0, jimd0)+
+            FFT_j[index3] = m_im(bat_s4r[0], bat_s4j[0], rimd0, jimd0)+
                     m_im(bat_s4r[1], bat_s4j[1], rimd1, jimd1)+
                     m_im(bat_s4r[2], bat_s4j[2], rimd2, jimd2)+
                     m_im(bat_s4r[3], bat_s4j[3], rimd3, jimd3);
 
-            // Multiply results on complex exponents
-            if (step!=16){
-                arg = 0;
-                exp_r[index0] = cos(arg);
-                exp_j[index0] = sin(-arg);
-
-                arg = (float)(2*pi*n)/(float)(N);
-                exp_r[index1] = cos(arg);
-                exp_j[index1] = sin(-arg);
-
-                arg = arg * 2;
-                exp_r[index2] = cos(arg);
-                exp_j[index2] = sin(-arg);
-
-                arg = arg * 3 / 2;
-                exp_r[index3] = cos(arg);
-                exp_j[index3] = sin(-arg);
+            rimd0=FFT_r[index0];
+            jimd0=FFT_j[index0];
 
 
-                rimd0=FFT_r[index0];
-                jimd0=FFT_j[index0];
+            rimd1=FFT_r[index1];
+            jimd1=FFT_j[index1];
 
 
-                rimd1=FFT_r[index1];
-                jimd1=FFT_j[index1];
+            rimd2=FFT_r[index2];
+            jimd2=FFT_j[index2];
 
 
-                rimd2=FFT_r[index2];
-                jimd2=FFT_j[index2];
+            rimd3=FFT_r[index3];
+            jimd3=FFT_j[index3];
 
+            FFT_r[index0] = m_real(rimd0, jimd0, exp_r[index0], exp_j[index0]);
+            FFT_j[index0] = m_im(rimd0, jimd0, exp_r[index0], exp_j[index0]);
 
-                rimd3=FFT_r[index3];
-                jimd3=FFT_j[index3];
+            FFT_r[index1] = m_real(rimd1, jimd1, exp_r[index1], exp_j[index1]);
+            FFT_j[index1] = m_im(rimd1, jimd1, exp_r[index1], exp_j[index1]);
 
-                FFT_r[index0] = m_real(rimd0, jimd0, exp_r[index0], exp_j[index0]);
-                FFT_j[index0] = m_im(rimd0, jimd0, exp_r[index0], exp_j[index0]);
+            FFT_r[index2] = m_real(rimd2, jimd2, exp_r[index2], exp_j[index2]);
+            FFT_j[index2] = m_im(rimd2, jimd2, exp_r[index2], exp_j[index2]);
 
-                FFT_r[index1] = m_real(rimd1, jimd1, exp_r[index1], exp_j[index1]);
-                FFT_j[index1] = m_im(rimd1, jimd1, exp_r[index1], exp_j[index1]);
-
-                FFT_r[index2] = m_real(rimd2, jimd2, exp_r[index2], exp_j[index2]);
-                FFT_j[index2] = m_im(rimd2, jimd2, exp_r[index2], exp_j[index2]);
-
-                FFT_r[index3] = m_real(rimd3, jimd3, exp_r[index3], exp_j[index3]);
-                FFT_j[index3] = m_im(rimd3, jimd3, exp_r[index3], exp_j[index3]);
-            }
+            FFT_r[index3] = m_real(rimd3, jimd3, exp_r[index3], exp_j[index3]);
+            FFT_j[index3] = m_im(rimd3, jimd3, exp_r[index3], exp_j[index3]);
     }
 
-    
-//
+/*
+for (kk=0; kk<64; kk+=16){
+    for (i=0; i<16; i++){
+        ear[i+kk]=exp_r[4*i];
+        eaj[i+kk]=exp_j[4*i];
+    }
+}*/
 
     // Perform second stage
     for (kk=0; kk<N; kk+=16){
+        
         for (step=1; step<=4; step*=4)
-        {
+        { 
             for (n=0; n<4; n++){
-
                     index0 = 0/step+n*step+kk;
                     index1 = 4/step+n*step+kk;
                     index2 = 8/step+n*step+kk;
@@ -234,23 +244,7 @@ int main()
 
                     // Multiply on complex expornents
                     if (step!=4){
-                        arg = 0;
-                        exp_r[index0] = cos(arg);
-                        exp_j[index0] = sin(-arg);
-
-                        arg = (float)(2*pi*n)/16.0;
-                        exp_r[index1] = cos(arg);
-                        exp_j[index1] = sin(-arg);
-
-                        arg = arg * 2;
-                        exp_r[index2] = cos(arg);
-                        exp_j[index2] = sin(-arg);
-
-                        arg = arg * 3 / 2;
-                        exp_r[index3] = cos(arg);
-                        exp_j[index3] = sin(-arg);
-
-
+                            
                         rimd0=FFT_r[index0];
                         jimd0=FFT_j[index0];
 
@@ -266,29 +260,50 @@ int main()
                         rimd3=FFT_r[index3];
                         jimd3=FFT_j[index3];
 
-                        FFT_r[index0] = m_real(rimd0, jimd0, exp_r[index0], exp_j[index0]);
-                        FFT_j[index0] = m_im(rimd0, jimd0, exp_r[index0], exp_j[index0]);
+                        FFT_r[index0] = m_real(rimd0, jimd0, exp_r[index0+d-kk], exp_j[index0+d-kk]);
+                        FFT_j[index0] = m_im(rimd0, jimd0, exp_r[index0+d-kk], exp_j[index0+d-kk]);
 
-                        FFT_r[index1] = m_real(rimd1, jimd1, exp_r[index1], exp_j[index1]);
-                        FFT_j[index1] = m_im(rimd1, jimd1, exp_r[index1], exp_j[index1]);
+                        FFT_r[index1] = m_real(rimd1, jimd1, exp_r[index1+d-kk], exp_j[index1+d-kk]);
+                        FFT_j[index1] = m_im(rimd1, jimd1, exp_r[index1+d-kk], exp_j[index1+d-kk]);
 
-                        FFT_r[index2] = m_real(rimd2, jimd2, exp_r[index2], exp_j[index2]);
-                        FFT_j[index2] = m_im(rimd2, jimd2, exp_r[index2], exp_j[index2]);
+                        FFT_r[index2] = m_real(rimd2, jimd2, exp_r[index2+d-kk], exp_j[index2+d-kk]);
+                        FFT_j[index2] = m_im(rimd2, jimd2, exp_r[index2+d-kk], exp_j[index2+d-kk]);
 
-                        FFT_r[index3] = m_real(rimd3, jimd3, exp_r[index3], exp_j[index3]);
-                        FFT_j[index3] = m_im(rimd3, jimd3, exp_r[index3], exp_j[index3]);
+                        FFT_r[index3] = m_real(rimd3, jimd3, exp_r[index3+d-kk], exp_j[index3+d-kk]);
+                        FFT_j[index3] = m_im(rimd3, jimd3, exp_r[index3+d-kk], exp_j[index3+d-kk]);
+                        
+                        d+=15;
                     }
             }
+            d=0;
         }
     }
 
 
+    for (i=0; i<N; i++){
+        FFT_j[i]=sqrt(FFT_r[i]*FFT_r[i]+FFT_j[i]*FFT_j[i]);
+    }
+
+    // Sort stage 
+    kk=0;
+    step=0;
+    while(kk<N){
+        for (i=0+step; i<4+step; i++){
+            FFT_r[4*i]=FFT_j[4*i-kk];
+            FFT_r[4*i+1]=FFT_j[4*i+16-kk];
+            FFT_r[4*i+2]=FFT_j[4*i+32-kk];
+            FFT_r[4*i+3]=FFT_j[4*i+48-kk];
+        }
+        step+=4;
+        kk+=15;
+    }
+
     // See results
     for (i=0; i<N; i++)
     {
-        printf(" %f", FFT_r[i]);
-        printf("  %f\n", FFT_j[i]);
+        printf(" %f\n", FFT_r[i]);
     }
+
     getch();
     return 0;
 }
